e0e4060ee2ade7c0aa614967956ec199b2c4772f
Faiyaz Ahmed <faiyaz@nextventures.io>
2026-07-20 15:44:19 +0600

    Implement theme detection in App component
    Add theme detection on app load using localStorage.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    try {
+      const t = localStorage.getItem('theme');
+      if (t === 'dark') document.documentElement.setAttribute('data-theme', 'dark');
+    } catch (e) {}
+  }, []);
   return <Component {...pageProps} />;
 }
