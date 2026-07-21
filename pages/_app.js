5472b14a8c5297366c61b48fce037ae1c69dc151
Faiyaz Ahmed <faiyaz@nextventures.io>
2026-07-21 16:48:02 +0600

    Enhance App component with Head for SEO
    Added Head component for SEO and theming.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import { useEffect } from 'react';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
@@ -8,5 +9,13 @@ export default function App({ Component, pageProps }) {
       if (t === 'dark') document.documentElement.setAttribute('data-theme', 'dark');
     } catch (e) {}
   }, []);
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>FundedNext Support Assistant</title>
+      <meta name="description" content="FundedNext internal support knowledge assistant" />
+      <meta name="theme-color" content="#6d4aff" />
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
